4a7a0b4cb6a2893a30c8fb01c64ccc0537300caf
linda <1630098@upv.edu.mx>
2019-05-27 05:30:26 +0000

    practica6

---
+++ models/database.php
@@ -0,0 +1,33 @@
+<?php
+
+	class Database{
+		private $con;
+		private $dbhost="localhost";
+		private $dbuser="root";
+		private $dbpass="zanahoria1teran";
+		private $dbname="hotel";
+    
+		function __construct(){
+			$this->connect_db();
+		}
+    
+		public function connect_db(){
+			$this->con = mysqli_connect($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
+			if(mysqli_connect_error()){
+				die("Conexión a la base de datos falló " . mysqli_connect_error() . mysqli_connect_errno());
+			}
+		}
+    
+    public function getCon(){
+      return $this->con;
+    }
+   
+    public function readUsuario(){
+      $sql = "SELECT * FROM usuarios";
+      $res = mysqli_query($this->con, $sql);
+    }
+  
+    
+  }
+
+?>
